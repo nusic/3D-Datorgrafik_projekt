6db5c688ac5956ff998678098c0250677cc1b5fd
Erik Broberg <erik_broberg_cool@hotmail.com>
2013-04-19 22:53:53 +0200

    project conventions

---
+++ CONVENTION.cpp
@@ -0,0 +1,22 @@
+
+ClassName{
+public:
+	bool methodName(int arg);
+
+private:
+	int bar;
+	static double StaticVar;
+	const bool CONST_VAR;
+}
+
+bool ClassName::methodName(int arg)
+{	
+	//comment 
+	return arg < ClassName::STATIC_VAR;
+}
+
+void function_name()
+{
+	do stuff;
+}
+
