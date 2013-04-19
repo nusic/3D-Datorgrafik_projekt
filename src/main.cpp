2ec0e74beb324e68870d9a2ffc6af57a3d062af0
kbladin <kallebladin@gmail.com>
2013-04-19 14:22:31 +0200

    Lade till en liten löjlig draw-funktion.

--- src/main.cpp
+++ src/main.cpp
@@ -31,4 +31,15 @@ int main( int argc, char* argv[] )
 
 void myDrawFun()
 {
-};
+	//render a single triangle
+	glBegin(GL_TRIANGLES);
+		glColor3f(1.0f, 0.0f, 0.0f); //Red
+		glVertex3f(-0.5f, -0.5f, 0.0f);
+ 
+		glColor3f(0.0f, 1.0f, 0.0f); //Green
+		glVertex3f(0.0f, 0.5f, 0.0f);
+ 
+		glColor3f(0.0f, 0.0f, 1.0f); //Blue
+		glVertex3f(0.5f, -0.5f, 0.0f);
+	glEnd();
+}
