af4b60b86e1597f97dc583f16d21b9c5f4bd879e
Erik Broberg <erik_broberg_cool@hotmail.com>
2013-04-20 14:44:44 +0200

    Model has pointer to ModelMesh

--- src/Model.cpp
+++ src/Model.cpp
@@ -1,6 +1,6 @@
 #include "Model.h"
 
-Model::Model(glm::mat4 M, ModelMesh _mesh): 
+Model::Model(glm::mat4 M, ModelMesh* _mesh): 
 modelMatrix(M), mesh(_mesh) {
 	std::cout << "created Model" << std::endl;
 }
--- src/Model.h
+++ src/Model.h
@@ -10,13 +10,13 @@
 
 class Model{
 public:
-	Model(glm::mat4 M = glm::mat4(1.0), ModelMesh mesh = NULL);
+	Model(glm::mat4 M = glm::mat4(1.0), ModelMesh* _mesh = NULL);
 
 	void drawModel(glm::mat4 MVP) const;
 
 	//transformation matrix specifik for this model
 	glm::mat4 modelMatrix;
-	ModelMesh mesh;
+	ModelMesh* mesh;
 	GLuint shaderProgramID;
 
 	std::vector<Model> children;
