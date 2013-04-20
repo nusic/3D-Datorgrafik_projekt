0a01ca265fa2e3c6fd04c099f321ce2534e6e925
Erik Broberg <erik_broberg_cool@hotmail.com>
2013-04-20 17:55:02 +0200

    added bös-folder

---
+++ bowZs/CONVENTION.cpp
@@ -0,0 +1,29 @@
+
+class ClassName{
+public:
+	bool methodName(int arg);
+
+private:
+	int* bar;
+	static double StaticVar;
+	const bool CONST_VAR;
+};
+
+bool ClassName::methodName(int arg){
+	code code code;
+
+	//comment above code
+	return arg < ClassName::STATIC_VAR;
+}
+
+int function_name(){
+	if(condition){
+		do stuff;
+		and some more;
+	}
+	else 
+		do other stuff;
+	
+	return (foo == bar) ? 3 : 4;
+}
+
