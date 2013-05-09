42d973df90fa0427e4c52f7676d224a2c0e1ffaa
Erik Broberg <erik_broberg_cool@hotmail.com>
2013-05-09 19:57:02 +0200

    avoiding null pointer exception

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -80,7 +80,7 @@ void Scene::initScene(){
 	addPlayer(body6);
 
 	std::string imgpath= "data/heightmap/heightmap_test.bmp";
-    readBMP(imgpath.c_str());
+    //readBMP(imgpath.c_str());
 }
 
 void Scene::addPlayer(Player * p){
@@ -113,39 +113,41 @@ float Scene::getZPosition(int x, int y){
 void Scene::readBMP(const char* filename)
 {
     FILE* f = fopen(filename, "rb");
-    unsigned char info[54];
-    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
+    if(f){
+	    unsigned char info[54];
+	    fread(info, sizeof(unsigned char), 54, f); // read the 54-byte header
 
-    // extract image height and width from header
-    int width = *(int*)&info[18];
-    int height = *(int*)&info[22];
+	    // extract image height and width from header
+	    int width = *(int*)&info[18];
+	    int height = *(int*)&info[22];
 
-    int size = 3 * width * height;
-    unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
-    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
-    fclose(f);
+	    int size = 3 * width * height;
+	    unsigned char* data = new unsigned char[size]; // allocate 3 bytes per pixel
+	    fread(data, sizeof(unsigned char), size, f); // read the rest of the data at once
+	    fclose(f);
 
-//    for(int i = 0; i < size; i += 3)
-//    {
-//            unsigned char tmp = data[i];
-//            data[i] = data[i+2];
-//            data[i+2] = tmp;
-//    }
+	//    for(int i = 0; i < size; i += 3)
+	//    {
+	//            unsigned char tmp = data[i];
+	//            data[i] = data[i+2];
+	//            data[i+2] = tmp;
+	//    }
 
-    heightmap = new float[width*height]; //new heightmap
+	    heightmap = new float[width*height]; //new heightmap
 
-    float scale = 0.1f;
-    for(int i = 0; i < width*height; i++)
-    {
-        heightmap[i] = scale*(float)data[i*3];
-    }
+	    float scale = 0.1f;
+	    for(int i = 0; i < width*height; i++)
+	    {
+	        heightmap[i] = scale*(float)data[i*3];
+	    }
 
-//    for(int i = 0; i < width; i++)
-//    {
-//        for(int j = 0; j < height; j++)
-//        {
-//            heightmap[i*j] = scale*(float)data[k];
-//            k += 3;
-//        }
-//    }
+	//    for(int i = 0; i < width; i++)
+	//    {
+	//        for(int j = 0; j < height; j++)
+	//        {
+	//            heightmap[i*j] = scale*(float)data[k];
+	//            k += 3;
+	//        }
+	//    }
+}
 }
