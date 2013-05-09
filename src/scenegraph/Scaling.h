857327e88de92bd966a0835b8368bfa8874e2474
Erik Broberg <erik_broberg_cool@hotmail.com>
2013-05-09 18:33:06 +0200

    deleting files in src/scenegraph

--- src/scenegraph/Scaling.h
+++
@@ -1,9 +0,0 @@
-#include "Transformation.h"
-
-class Scaling : public Transformation {
-public:
-	Scaling(float sx, float sy, float sz);
-	virtual ~Scaling();
-
-	void setScaling(float sx, float sy, float sz);
-};
